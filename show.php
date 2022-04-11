7d10135680e57616e39e405e786a1083a643ab55
EC2 Default User <ec2-user@ip-172-31-12-137.ap-northeast-1.compute.internal>
2022-04-11 11:33:46 +0900

    余計なコメントをさらに削除

--- show.php
+++ show.php
@@ -2,12 +2,9 @@
     // (C)
     require_once 'models/User.php';
     session_start();
-    // var_dump($_GET);
     $id = $_GET['id'];
-    // print $id;
     // データベースから指定されたIDのデータを取得
     $user = User::find($id);
     $token = session_id();
-    // var_dump($user);
     // Viewの表示
     include_once 'views/show_view.php';
